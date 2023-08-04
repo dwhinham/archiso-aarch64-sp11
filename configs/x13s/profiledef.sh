67344a0c41068c691c51711cdf3da525a633737f
Clover <alex@ironrobin.net>
2023-08-03 19:38:38 -0500

    Release-08-03-2023 (#4)
    * add ironrobin-setup script
    
    * add support for workman keymap
    
    * remove community repo from pacman.conf

--- configs/x13s/profiledef.sh
+++ configs/x13s/profiledef.sh
@@ -21,4 +21,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/ironrobin-setup"]="0:0:755"
+  ["/usr/local/bin/workman"]="0:0:755"
 )
