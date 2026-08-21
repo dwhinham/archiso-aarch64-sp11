44242b17a43d5a8a8a84265ac143418561aa2b3b
Clover <alex@ironrobin.net>
2026-08-21 13:44:42 -0500

    add aarch64 boot compat params + add custom pacman keys for archports / ironrobin

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -12,8 +12,9 @@ bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+kernel_params_aarch64="clk_ignore_unused pd_ignore_unused arm64.nopauth systemd.mask=tpm2.target"
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
