2341771720e080cd0f605475f80ad49b327e4f49
Clover <alex@ironrobin.net>
2026-09-01 11:21:47 -0500

    fix supported device list

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -3,8 +3,8 @@
 
 iso_name="archlinux"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_publisher="Clover <alex@ironrobin.net>"
+iso_application="Arch Linux Ports - aarch64 Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
